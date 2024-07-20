ac51822ecbd2357b17bd0fe5126e33004c97762a
Ghan-sudo <ghan46275@gmail.com>
2024-07-21 02:19:25 +0530

    Trying again 256e2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "./transcoderservice/",
+  base : "./",
   plugins: [react(),tailwindcss()],
 })
